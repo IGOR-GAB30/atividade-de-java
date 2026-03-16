8bc02fd19a2c949f0054bb20dcf520902a2ed1fd
IGOR-GAB30 <igor_paradella@estudante.sesisenai.org.br>
2026-03-15 22:41:39 -0300

    ADD - mais o exercício 6 concluído. Nele o sistema requisita um caractere para a variável letra. Após a atribuição o sistema precisou de um sistema if/else para verificar se a letra era igual a alguma das 5 vogais, caso contrário ela seria uma consoante. Resultado no console.

--- script.js
+++ script.js
@@ -166,6 +166,27 @@ function verificarNumero() {
 
 }
 
-verificarNumero();
+//verificarNumero();
+
+//exercício 6: Verificador de Vogal ou Consoante
+
+function VogalouConsoante() {
+
+ let letra = prompt("Digite uma letra: ");
+
+    if(letra === "a" || letra === "A" || letra === "e" || letra === "E" || letra === "i" || letra === "I" || letra ==="o" || letra === "O" || letra ==="u" || letra === "U" ){
+ 
+        console.log("A letra '" + letra + "' é vogal");
+
+    } else{
+
+        console.log("A letra '" + letra + "' é consoante");
+    }
+
+}
+
+VogalouConsoante();
+
+//exercício 7: 
 
 
